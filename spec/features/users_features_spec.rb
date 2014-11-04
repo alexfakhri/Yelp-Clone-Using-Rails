e1587b29fcb6356aef3aff5b5abe9e7de1debde8
alexfakhri <alexfakhri@me.com>
2014-11-04 15:59:10 +0000

    Adding user sign out features

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -14,3 +14,27 @@ context "User not signed in and on the homepage" do
 	end
 
 end
+
+context "User signed in on the home page" do
+
+	before do
+		visit '/'
+		click_link('Sign up')
+		fill_in('Email', with: 'test@test.com')
+		fill_in('Password', with: 'test1234')
+		fill_in('Password confirmation', with: 'test1234')
+		click_button('Sign up')
+	end
+
+	it "Should see 'sign out' link" do
+		visit '/'
+		expect(page).to have_link('Sign out')
+	end
+
+	it "should not see a 'sign in' link and a 'sign up' link" do
+		visit '/'
+		expect(page).not_to have_link('Sign in')
+		expect(page).not_to have_link('Sign up')
+	end
+	
+end
