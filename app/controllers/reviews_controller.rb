f0868973c6918c9031c2f33cb7c033d656d63eb8
alexfakhri <alexfakhri@me.com>
2014-11-05 17:50:08 +0000

    Users are not allowed not review a restaurant more than once

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,14 @@ class ReviewsController < ApplicationController
 
 	def create
 		@restaurant = Restaurant.find(params[:restaurant_id])
-		@restaurant.reviews.create(params[:review].permit(:thoughts, :rating))
-		redirect_to restaurants_path
+		@review = @restaurant.reviews.new(params[:review].permit(:thoughts, :rating))
+		@review.user = current_user
+		if @review.save
+			redirect_to restaurants_path
+		else
+			flash[:notice] = "You've already reviewed this restaurant"
+			redirect_to '/restaurants'
+		end
 	end
 
 end
