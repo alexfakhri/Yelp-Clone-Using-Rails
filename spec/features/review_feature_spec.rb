7710dd108371003b6298915469d301793d59d3af
NicolePell <pellicena@gmail.com>
2014-11-06 11:05:11 +0000

    Restaurant displays average rating

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -2,26 +2,37 @@ require 'rails_helper'
 
 describe 'reviewing' do
 	before do
-		Restaurant.create(name: 'KFC')
-		visit '/restaurants'
-		click_link 'Review KFC'
-		fill_in "Thoughts", with: "so so"
-		select '3', from: 'Rating'
-		click_button 'Leave Review'
+		@restaurant = Restaurant.create(name: 'KFC')
+		def leave_review(thoughts, rating)
+			visit '/restaurants'
+			click_link 'Review KFC'
+			fill_in "Thoughts", with: "so so"
+			select '3', from: 'Rating'
+			click_button 'Leave Review'
+		end
 	end
 
 	it 'allows users to leave a review using a form' do
-		expect(current_path).to eq '/restaurants'
+		visit '/restaurants'
+		leave_review('so so', "2")
 		expect(page).to have_content('so so')
 	end
 
 	it 'should not allow a user to leave more than one review per restaurant' do
 		visit '/restaurants'
+		leave_review('so so', '2')
 		click_link 'Review KFC'
 		fill_in "Thoughts", with: "so so"
 		select '3', from: 'Rating'
 		click_button 'Leave Review'
 		expect(page).to have_content("You've already reviewed this restaurant")
+	end
+
+	it 'displays an average rating for all reviews' do
+		Review.create(thoughts: 'so so', rating: 3, user_id: 1, restaurant: @restaurant)
+		Review.create(thoughts: 'great', rating: 5, user_id: 2, restaurant: @restaurant)
+		visit '/restaurants'
+		expect(page).to have_content("Average rating: 4")
 	end
 
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-RSpec.describe Restaurant, :type => :model do
-  
+RSpec.describe Restaurant, type: :model do
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
@@ -12,6 +12,32 @@ RSpec.describe Restaurant, :type => :model do
   	Restaurant.create(name: "Moe's Tavern")
   	restaurant = Restaurant.new(name: "Moe's Tavern")
   	expect(restaurant).to have(1).error_on(:name)
+  end
+
+  describe "#average_rating" do
+    context 'no reviews' do
+      it 'returns "N/A" when there are no reviews' do
+        restaurant = Restaurant.create(name: "The Ivy")
+        expect(restaurant.average_rating).to eq 'N/A'
+      end
+    end
+
+    context '1 review' do
+      it 'returns that rating' do
+        restaurant = Restaurant.create(name: "The Ivy")
+        restaurant.reviews.create(rating: 4)
+        expect(restaurant.average_rating).to eq 4
+      end
+    end
+
+    context 'multiple reviews' do
+      it 'returns the average' do
+        restaurant = Restaurant.create(name: "The Ivy")
+        restaurant.reviews.create(rating: 1, user_id: 1)
+        restaurant.reviews.create(rating: 5, user_id: 2)
+        expect(restaurant.average_rating).to eq 3
+      end
+    end
   end
 
 end
